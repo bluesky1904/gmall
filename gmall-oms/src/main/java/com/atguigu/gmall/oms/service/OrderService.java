49e9fe03fa7914117e444e42971c5169622fce03
zyy <2414682144@qq.com>
2020-05-20 01:50:11 +0800

    逆向工程所有功能模块

---
+++ gmall-oms/src/main/java/com/atguigu/gmall/oms/service/OrderService.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.oms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.gmall.common.bean.PageResultVo;
+import com.atguigu.gmall.common.bean.PageParamVo;
+import com.atguigu.gmall.oms.entity.OrderEntity;
+
+import java.util.Map;
+
+/**
+ * 订单
+ *
+ * @author fengge
+ * @email fengge@atguigu.com
+ * @date 2020-05-20 01:26:06
+ */
+public interface OrderService extends IService<OrderEntity> {
+
+    PageResultVo queryPage(PageParamVo paramVo);
+}
+
